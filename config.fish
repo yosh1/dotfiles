26d3d7bfc7d4fb58d9b0c6074fd6d48b2e8fb686
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-10-14 08:27:06 +0900

    [Update] Add fish config.

---
+++ config.fish
@@ -0,0 +1,9 @@
+set fish_theme yimmy
+alias sb="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+alias vi='/usr/bin/vim'
+alias rm='rmtrash'
+alias docker='sudo docker'
+alias ga='git add .'
+alias gi='git init'
+alias gpm='git push origin master'
+alias gc='git commit -m'
