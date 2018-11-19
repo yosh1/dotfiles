afd92e27b837f4c0494935abd6a3c35b89562e70
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-11-19 16:00:49 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,12 +1,6 @@
 # fish theme
 set fish_theme yimmy
 
-# trash command
-alias rm='rmtrash'
-
-# Linux Command
-alias mk='touch'
-
 # Git Command
 alias ga "git add"
 alias gp "git push"
@@ -26,22 +20,22 @@ alias vi "vim"
 # Docker
 alias dcub "sudo docker-compose up --build"
 alias dcu "sudo docker-compoase up"
-alias d="sudo docker"
-alias de="sudo docker exec"
-alias db="sudo docker build"
-alias dc="sudo docker commit"
-alias di="sudo docker images"
-alias dip="sudo docker inspect"
-alias ds="sudo docker search"
-alias dst="sudo docker stop"
-alias dpl="sudo docker pull"
-alias dps="sudo docker ps"
-alias drm='sudo docker rm $(docker ps -aq)'
-alias drmi="sudo docker rmi"
-alias dv="sudo docker volume"
+alias d "sudo docker"
+alias de "sudo docker exec"
+alias db "sudo docker build"
+alias dc "sudo docker commit"
+alias di "sudo docker images"
+alias dip "sudo docker inspect"
+alias ds "sudo docker search"
+alias dst "sudo docker stop"
+alias dpl "sudo docker pull"
+alias dps "sudo docker ps"
+alias drm 'sudo docker rm $(docker ps -aq)'
+alias drmi "sudo docker rmi"
+alias dv "sudo docker volume"
 
 # docker utils
-dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)='\(.*\)'/\1    => \2/"| sed "s/'\\\'//g"; }
+# dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)='\(.*\)'/\1    => \2/"| sed "s/'\\\'//g"; }
 
 # npm
 alias npm "sudo npm"
