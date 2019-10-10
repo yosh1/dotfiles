7bd840ccfe8a3dd9739b27c5983eeb800f779840
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-10-10 12:00:00 +0900

    Add xcode-uninstall command

---
+++ mac/xcode-unintall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo rm -rf /Applications/Xcode.app
+sudo rm -rf /Library/Preferences/com.apple.dt.Xcode.plist
+sudo rm -rf ~/Library/Preferences/com.apple.dt.Xcode.plist
+sudo rm -rf ~/Library/Caches/com.apple.dt.Xcode
+sudo rm -rf ~/Library/Application Support/Xcode
+sudo rm -rf ~/Library/Developer/Xcode
+sudo rm -rf ~/Library/Developer/CoreSimulator
