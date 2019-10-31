38bd4e6f2d6f79dd94fe091d03395d394ea4bf2f
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-10-31 12:00:00 +0900

    update fish.conf

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,41 @@
-set -g -x PATH /usr/local/bin $PATH
+# Init PATH
+set -x PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+
+set -x LANG "ja_JP.UTF-8"
+
+# Fish
+set -x fish_greeting ""
+
+# Git
+function commit_empty 
+  git commit --allow-empty -m "NOPR: squash me [ci skip]"
+end
+
+function gitco
+  set argv $argv "-"
+  git checkout $argv[1]
+end
+
+function hp
+  history | peco
+end
+
+echo "config.fish is loaded."
+
+# Flutter
+set PATH $HOME/flutter/bin $PATH
+
+# Android 
+set PATH /Users/yoshi1125hisa/Library/Android/sdk/platform-tools $PATH
+
+# GOPATH
+set -Ux GOPATH $HOME/Desktop/Go
 
 # fish theme
-set fish_theme yimmy
+# set fish_theme yimmy
+
+# Linux Command
+alias mk='touch'
 
 # Git Command
 alias ga "git add"
@@ -13,30 +47,18 @@ alias p3 "python3"
 alias python "python3"
 alias pip "pip3"
 
-# Vim
-alias vi "vim"
-
 # Docker
 alias dcub "sudo docker-compose up --build"
-alias dcu "sudo docker-compoase up"
-alias d "sudo docker"
-alias de "sudo docker exec"
-alias db "sudo docker build"
-alias dc "sudo docker commit"
-alias di "sudo docker images"
-alias dip "sudo docker inspect"
-alias ds "sudo docker search"
-alias dst "sudo docker stop"
-alias dpl "sudo docker pull"
-alias dps "sudo docker ps"
-alias drmi "sudo docker rmi"
-alias dv "sudo docker volume"
 
-# docker utils
-# dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)='\(.*\)'/\1    => \2/"| sed "s/'\\\'//g"; }
+# mongodb
+set PATH /Users/yoshi1125hisa/mongodb/bin $PATH
 
-# npm
-alias npm "sudo npm"
+# rbenv
+set PATH "$HOME/.rbenv/bin" $PATH
+. (rbenv init - | psub)
 
-#yarn
-alias yarn "sudo yarn"
+# flutter
+set -x PATH /Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin /Users/yoshi1125hisa/flutter/bin:/Users/yoshi1125hisa/.rbenv/shims:/Users/yoshi1125hisa/.pyenv/shims:/Users/yoshi1125hisa/.nodebrew/current/bin/:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/share/dotnet:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/Applications/Xamarin:Workbooks.app/Contents/SharedSupport/path-bin
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/yoshi1125hisa/google-cloud-sdk/path.fish.inc' ]; . '/Users/yoshi1125hisa/google-cloud-sdk/path.fish.inc'; end
